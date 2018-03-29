405d5df607d6ddda69f06e623f03cc20299b0248
Prakhar Gupta <prakharguptaiitd@gmail.com>
2018-03-30 01:57:48 +0530

    Added function to create media

--- Media.sol
+++ Media.sol
@@ -3,24 +3,27 @@ pragma solidity ^0.4.21;
 contract Media {
 
   struct media {
-    bytes32 creator;
-    bytes32 mediaId;
+    address creator;
+    uint mediaId;
     uint priceIndividual;
     uint priceCompany;
-    // TODO
+    // TODO: Add stakeholders
+    // TODO: Add other things
   }
   
   media[] private mediaList;
-  mapping (bytes32 => media[]) private owns;
-  mapping (bytes32 => bool) private isCompany;
-  mapping (bytes32 => bool) private isCreator;
-  bytes32[] public userList;
+  mapping (address => media[]) private owns;
+  mapping (address => bool) private isCompany;
+  mapping (address => bool) private isCreator;
 
-  function Media(bytes32[] users) public {
-    userList = users;
+  function Media() public {
   }
 
-  function hasSeenMedia(bytes32 user, media testMedia) view private returns (bool) {
+  function isNewUser(address user) view private returns (bool) {
+    return (isCreator[user].length ==  0);
+  }
+
+  function hasSeenMedia(address user, media testMedia) view private returns (bool) {
     media[] userOwned = owns[user];
     for(uint i = 0; i < userOwned.length; i++) {
       if (userOwned[i] == testMedia) {
@@ -30,8 +33,14 @@ contract Media {
     return false;
   }
 
-  function availableMedia(bytes32 user) view public returns (media[]) {
-    require(validCandidate(user));
+  function availableMedia() view public returns (media[]) {
+    address user = msg.sender;
+    if (isNewUser(user)) {
+      isCreator[user] = false;
+      // TODO: generate random no.
+      isCompany[user] = false;
+    }
+    require(isCreator[user]);
     media[] retMedia;
     for(uint i = 0; i < mediaList.length; i++) {
       if (!hasSeenMedia(user, mediaList[i])) {
@@ -41,12 +50,18 @@ contract Media {
     return retMedia;
   }
 
-  function validUser(bytes32 user) view private returns (bool) {
-    for(uint i = 0; i < userList.length; i++) {
-      if (userList[i] == user) {
-        return true;
-      }
+  function addMedia(uint newMediaId, uint newPriceIndividual, uint newPriceCompany) public {
+    address user = msg.sender;
+    if (isNewUser(user)) {
+      isCreator[user] = true;
     }
-    return false;
-   }
+    require(!isCreator[user]);
+    media newEntry = media({
+      creator: user,
+      mediaId: newMediaId,
+      priceIndividual: newPriceIndividual,
+      priceCompany; newPriceCompany
+    });
+    mediaList.push(newEntry);
+  }
 }
