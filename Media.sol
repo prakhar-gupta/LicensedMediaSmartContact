5783e15ca4182d4cf91ddf01b564cc8c87aa7e34
Prakhar Gupta <prakharguptaiitd@gmail.com>
2018-03-30 02:45:46 +0530

    Partial initiateBuy

--- Media.sol
+++ Media.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.21;
 
-contract Media {
+contract MediaContract {
 
-  struct media {
+  struct Media {
     address creator;
     uint mediaId;
     uint priceIndividual;
@@ -11,20 +11,27 @@ contract Media {
     // TODO: Add other things
   }
   
-  media[] private mediaList;
-  mapping (address => media[]) private owns;
+  enum State { Created, Locked, Inactive }
+
+  Media[] private mediaList;
+  mapping (uint => bool) private mediaExists;
+  mapping (address => Media[]) private owns;
   mapping (address => bool) private isCompany;
   mapping (address => bool) private isCreator;
+  mapping (address => mapping (mediaId => State)) private txnState;
+  
+  event InitBuy(address buyer, Media media);
 
-  function Media() public {
+  function MediaContract() public {
   }
 
   function isNewUser(address user) view private returns (bool) {
     return (isCreator[user].length ==  0);
   }
 
-  function hasSeenMedia(address user, media testMedia) view private returns (bool) {
-    media[] userOwned = owns[user];
+  function hasSeenMedia(address user, Media testMedia) view private returns (bool) {
+    require(mediaExists(testMedia).length == 1);
+    Media[] userOwned = owns[user];
     for(uint i = 0; i < userOwned.length; i++) {
       if (userOwned[i] == testMedia) {
         return true;
@@ -33,7 +40,7 @@ contract Media {
     return false;
   }
 
-  function availableMedia() view public returns (media[]) {
+  function availableMedia() view public returns (Media[]) {
     address user = msg.sender;
     if (isNewUser(user)) {
       isCreator[user] = false;
@@ -41,7 +48,7 @@ contract Media {
       isCompany[user] = false;
     }
     require(isCreator[user]);
-    media[] retMedia;
+    Media[] retMedia;
     for(uint i = 0; i < mediaList.length; i++) {
       if (!hasSeenMedia(user, mediaList[i])) {
         retMedia.push(mediaList[i]);
@@ -56,12 +63,20 @@ contract Media {
       isCreator[user] = true;
     }
     require(!isCreator[user]);
-    media newEntry = media({
+    Media newEntry = Media({
       creator: user,
       mediaId: newMediaId,
       priceIndividual: newPriceIndividual,
       priceCompany; newPriceCompany
     });
     mediaList.push(newEntry);
+    mediaExists[newEntry] = 1;
+  }
+
+  function initiateBuy(Media media) public {
+    address user = msg.sender;
+    txnState[user][media.mediaId] = State.Created;
+    // TODO: lockin amount
+    emit InitBuy(user, media);
   }
 }
